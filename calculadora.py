0c7be80943b49b377c15172c56479e0bf8fdd794
Airon <86085059+Air0N@users.noreply.github.com>
2022-06-15 11:28:04 -0300

    cal.

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Calculadora:
     def soma(self, a, b):
         return a + b
@@ -9,4 +11,39 @@ class Calculadora:
         return a * b
 
     def div(self, a, b):
-        return a / b
+        if b != 0:
+            return a / b
+
+class Teste(unittest.TestCase):
+
+    def setup(self):
+        self.Calculadora = ('Calculadora')
+
+
+    def teste_soma(self):
+        self.calc = Calculadora()
+        resultado = self.calc.soma(1, 2)
+        esperado = 3
+        self.assertEqual(resultado, esperado)
+
+    def teste_sub(self):
+        self.calc = Calculadora()
+        resultado = self.calc.sub(18, 12)
+        esperado = 6
+        self.assertEqual(resultado, esperado)
+
+    def teste_multi(self):
+        self.calc = Calculadora()
+        resultado = self.calc.mult(5, 5)
+        esperado = 25
+        self.assertEqual(resultado, esperado)
+
+    def teste_div(self):
+        self.calc = Calculadora()
+        resultado = self.calc.div(24, 2)
+        esperado = 12
+        self.assertEqual(resultado, esperado)
+
+
+if __name__ == '__main__':
+    unittest.main()
